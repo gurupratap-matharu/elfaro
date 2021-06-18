986cc17e9b25eb9a832ee271f0fe9d2c2a5dd6a4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-18 18:40:17 -0300

    Add Subject group to factories

--- classroom/factories.py
+++ classroom/factories.py
@@ -43,8 +43,55 @@ class TeacherFactory(factory.django.DjangoModelFactory):
 
 
 class StudentFactory(factory.django.DjangoModelFactory):
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    dni = factory.Faker("random_int", min=10000000, max=99999999)
+    date_of_birth = factory.Faker(
+        "date_of_birth",
+        tzinfo=timezone.get_current_timezone(),
+        minimum_age=15,
+        maximum_age=40,
+    )
+    date_of_admission = factory.Faker("date_this_decade")
+    email = factory.LazyAttribute(
+        lambda obj: "%s@example.com" % (obj.first_name.lower() + obj.last_name.lower())
+    )
+    address1 = factory.Faker("address")
+    address2 = factory.Faker("address")
+    city = factory.Faker("city")
+    gender = factory.Faker("random_element", elements=GENDER_CHOICES)
+    marital_status = factory.Faker("random_element", elements=MARITAL_STATUS_CHOICES)
+    phone_number = factory.LazyAttribute(lambda o: "+54911%s" % o.phone_num)
+    permission_for_photo = factory.Faker("boolean")
+    father_first_name = factory.Faker("first_name")
+    father_last_name = factory.Faker("last_name")
+    father_dni = factory.Faker("random_int", min=10000000, max=99999999)
+    father_email = factory.LazyAttribute(
+        lambda obj: "%s@example.com"
+        % (obj.father_first_name.lower() + obj.father_last_name.lower())
+    )
+    mother_first_name = factory.Faker("first_name")
+    mother_last_name = factory.Faker("last_name")
+    mother_dni = factory.Faker("random_int", min=10000000, max=99999999)
+    mother_email = factory.LazyAttribute(
+        lambda obj: "%s@example.com"
+        % (obj.mother_first_name.lower() + obj.mother_last_name.lower())
+    )
+    active = factory.Faker("boolean")
+
     class Meta:
         model = Student
+
+    class Params:
+        phone_num = factory.Faker("randomize_nb_elements", number=50254191)
+
+
+class SubjectFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker("catch_phrase")
+    active = factory.Faker("boolean")
+
+    class Meta:
+        model = Subject
 
 
 class CourseFactory(factory.django.DjangoModelFactory):
@@ -52,11 +99,10 @@ class CourseFactory(factory.django.DjangoModelFactory):
         model = Course
 
 
-class SubjectFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Subject
-
-
 class SubjectGroupFactory(factory.django.DjangoModelFactory):
+    name = factory.Sequence(lambda n: "Group #%s" % n)
+    year = factory.Faker("random_int", min=1, max=6)
+    active = factory.Faker("boolean")
+
     class Meta:
         model = SubjectGroup
