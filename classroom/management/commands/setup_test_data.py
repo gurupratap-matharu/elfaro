4c3ba58eb429912f38ceaaad33e424eddb404d37
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-18 18:46:19 -0300

    added color to critical message of deletion

--- classroom/management/commands/setup_test_data.py
+++ classroom/management/commands/setup_test_data.py
@@ -1,18 +1,35 @@
+"""A utility script to load test data into the db"""
+
 from django.core.management.base import BaseCommand
 
-from classroom.factories import TeacherFactory
-from classroom.models import Teacher
+from classroom.factories import StudentFactory, SubjectFactory, TeacherFactory
+from classroom.models import Student, Subject, Teacher
 
 
 class Command(BaseCommand):
+    """Management command which cleans and populates database with mock data"""
+
     help = "Loads fake data into the database"
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.HTTP_INFO("Deleting old data..."))
+        self.stdout.write(self.style.HTTP_BAD_REQUEST("Deleting old data..."))
         Teacher.objects.all().delete()
+        Student.objects.all().delete()
+        Subject.objects.all().delete()
 
         self.stdout.write(self.style.SUCCESS("Creating new data..."))
         for _ in range(20):
             TeacherFactory()
+            SubjectFactory()
 
-        self.stdout.write(self.style.SUCCESS("All done! 💖"))
+        for _ in range(100):
+            StudentFactory()
+
+        print(
+            f"""
+        Teachers: {Teacher.objects.count()}
+        Students: {Student.objects.count()}
+        Subjects: {Subject.objects.count()}
+        """
+        )
+        self.stdout.write(self.style.SUCCESS("All done! 💖💅🏻💫"))
