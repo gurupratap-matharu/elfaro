ced9e94fcad0aae38ed15ca9fde4dec6326ac61e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-19 12:25:57 -0300

    Add locale as a long option

--- classroom/management/commands/setup_test_data.py
+++ classroom/management/commands/setup_test_data.py
@@ -2,6 +2,7 @@
 
 import random
 
+import factory
 from django.core.management.base import BaseCommand
 
 from classroom.factories import (
@@ -19,7 +20,15 @@ class Command(BaseCommand):
 
     help = "Loads fake data into the database"
 
+    def add_arguments(self, parser) -> None:
+        parser.add_argument(
+            "-l", "--locale", type=str, help="Define a locale for the data to be generated."
+        )
+
     def handle(self, *args, **kwargs):
+        locale = kwargs.get("locale")
+        self.stdout.write(self.style.SUCCESS("Locale: %s" % locale))
+
         self.stdout.write(self.style.HTTP_BAD_REQUEST("Deleting old data..."))
         Teacher.objects.all().delete()
         Student.objects.all().delete()
@@ -29,17 +38,18 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS("Creating new data..."))
 
-        teachers = TeacherFactory.create_batch(size=20)
-        students = StudentFactory.create_batch(size=100)
-        subject_groups = SubjectGroupFactory.create_batch(size=5)
+        with factory.Faker.override_default_locale(locale):
+            teachers = TeacherFactory.create_batch(size=20)
+            students = StudentFactory.create_batch(size=100)
+            subject_groups = SubjectGroupFactory.create_batch(size=5)
 
-        for _ in range(20):
-            SubjectFactory(groups=random.choices(subject_groups, k=3))
-            CourseFactory(
-                students=random.choices(students, k=10),
-                teachers=random.choices(teachers, k=3),
-                subject_group=random.choice(subject_groups),
-            )
+            for _ in range(20):
+                SubjectFactory(groups=random.choices(subject_groups, k=3))
+                CourseFactory(
+                    students=random.choices(students, k=10),
+                    teachers=random.choices(teachers, k=3),
+                    subject_group=random.choice(subject_groups),
+                )
 
         print(
             f"""
