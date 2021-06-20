d8a1d956ff3f695c93c8d666ad18832d8e4d68a4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-20 16:35:33 -0300

    Route all authentication to allauth app

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("allauth.urls")),
+    # Local apps
     path("", include("classroom.urls")),
 ]
