c2ad9dabb18605470d8b21287fe68292911ef910
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-24 18:55:54 -0300

    Introduce abstract model to reduce repeated fields

--- classroom/models.py
+++ classroom/models.py
@@ -1,11 +1,16 @@
+"""Database models for the elfaro platform"""
+
 import uuid
 
+from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Teacher(models.Model):
+class Person(models.Model):
+    """Abstract class which represents a person with common fields"""
+
     MALE = "M"
     FEMALE = "F"
     OTHER = "O"
@@ -76,15 +81,25 @@ class Teacher(models.Model):
         max_length=17,
         blank=True,
     )
+
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(Person):
+    """Teacher model which inherits from the Person abstract model."""
+
     certified = models.BooleanField(
         verbose_name=_("Certified"),
         help_text=_("Are you certified as a teacher?"),
         default=False,
     )
-
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _("Teacher")
@@ -95,76 +110,9 @@ class Teacher(models.Model):
         return ", ".join([self.first_name, self.last_name, str(self.dni)])
 
 
-class Student(models.Model):
-    MALE = "M"
-    FEMALE = "F"
-    OTHER = "O"
+class Student(Person):
+    """Student model which inherits from the abstract Person model."""
 
-    GENDER_CHOICES = [
-        (MALE, _("Male")),
-        (FEMALE, _("Female")),
-        (OTHER, _("Other")),
-    ]
-
-    SINGLE = "S"
-    MARRIED = "M"
-    DIVORCED = "D"
-    WIDOWED = "W"
-
-    MARITAL_STATUS = [
-        (SINGLE, _("Single")),
-        (MARRIED, _("Married")),
-        (DIVORCED, _("Divorced")),
-        (WIDOWED, _("Widowed")),
-    ]
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(verbose_name=_("First name"), max_length=200)
-    last_name = models.CharField(verbose_name=_("Last name"), max_length=200)
-    dni = models.PositiveIntegerField(
-        verbose_name=_("DNI"), help_text=_("Your identification number")
-    )
-    date_of_birth = models.DateField(
-        verbose_name=_("Date of birth"),
-        help_text=_("Your date of birth as it appears on your national document."),
-    )
-    date_of_admission = models.DateField(
-        verbose_name=_("Date of admission"),
-        help_text=_("The date your registered with us."),
-    )
-    email = models.EmailField(
-        verbose_name=_("Your work email"),
-        help_text=_("We will send notification emails to this email address."),
-    )
-    address1 = models.CharField(verbose_name=_("Address line 1"), max_length=200)
-    address2 = models.CharField(verbose_name=_("Address line 2"), max_length=200)
-    city = models.CharField(verbose_name=_("City"), max_length=60)
-    gender = models.CharField(
-        verbose_name=_("Gender"),
-        max_length=2,
-        choices=GENDER_CHOICES,
-        default=FEMALE,
-    )
-    marital_status = models.CharField(
-        verbose_name=_("Marital Status"),
-        max_length=2,
-        choices=MARITAL_STATUS,
-        default=SINGLE,
-    )
-    phone_regex = RegexValidator(
-        regex=r"^\+?1?\d{9,15}$",
-        message=_(
-            "Phone number must be entered in the format: '+5491123456789'. "
-            "Up to 15 digits allowed. No spaces"
-        ),
-    )
-    phone_number = models.CharField(
-        verbose_name=_("Your cellphone number."),
-        help_text=_("We might drop you a message incase of an emergency"),
-        validators=[phone_regex],
-        max_length=17,
-        blank=True,
-    )
     permission_for_photo = models.BooleanField(
         verbose_name=_("Grant permission for photograph."),
         help_text=_("Whether you give us consent to use your photograph."),
@@ -207,10 +155,6 @@ class Student(models.Model):
         ),
         blank=True,
     )
-
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _("Student")
