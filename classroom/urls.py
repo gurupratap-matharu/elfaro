ab8f3022a58606f4fd5a1db34aa93c23af400026
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-06-25 15:47:16 -0300

    Add endpoint for course list+detail

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import CourseDetailView, CourseListView, HomePageView
 
 app_name = "classroom"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="classroom_home"),
+    path("courses/", CourseListView.as_view(), name="course_list"),
+    path("courses/<uuid:pk>/", CourseDetailView.as_view(), name="course_detail"),
 ]
